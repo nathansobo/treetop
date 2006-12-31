da3233d34151481c97d88aba3b43bf3075643a09
nathansobo <nathansobo@0990e5dc-4a6a-4c17-b0d6-4743f9744c17>
2006-12-31 03:16:52 +0000

    Cleaned up nonterminal.rb header; Changed scoped module definition to nested module definition.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/treetop@2 0990e5dc-4a6a-4c17-b0d6-4743f9744c17

--- trunk/lib/treetop/nonterminal.rb
+++ trunk/lib/treetop/nonterminal.rb
@@ -1,80 +1,76 @@
-dir = File.dirname(__FILE__)
-require "#{dir}/alternative"
-require "#{dir}/terminal"
-
-module Treetop::Nonterminal
+module Treetop
+  module Nonterminal
+    def self.create_nonterminal_class(name, containing_parser)
+      nonterminal_class = Class.new
+      nonterminal_class.extend(NonterminalClassMethods)
+      nonterminal_class.instance_eval do
+        include NonterminalInstanceMethods
+      end
+      nonterminal_class.name = name
+      nonterminal_class.containing_parser = containing_parser
+      return nonterminal_class
+    end
   
-  def self.create_nonterminal_class(name, containing_parser)
-    nonterminal_class = Class.new
-    nonterminal_class.extend(NonterminalClassMethods)
-    nonterminal_class.instance_eval do
-      include NonterminalInstanceMethods
-    end
-    nonterminal_class.name = name
-    nonterminal_class.containing_parser = containing_parser
-    return nonterminal_class
-  end
-  
-  module NonterminalClassMethods
-    attr_accessor :alternatives, :containing_parser, :name
+    module NonterminalClassMethods
+      attr_accessor :alternatives, :containing_parser, :name
 
-    def nonterminal?
-      true
-    end
-      
-    def terminal?
-      false
-    end
-    
-    def add_alternative(alt)
-      self.alternatives ||= Array.new
-      alt.containing_nonterminal = self
-      alternatives << alt
-    end
-
-    def parse_at(start, buffer, parser_instance)
-      if cached_result = parser_instance.node_cache.node_starting_at(self.name, start)
-        return cached_result
+      def nonterminal?
+        true
       end
       
-      for alt in alternatives do
-        parse_result = alt.parse_at(start, buffer, parser_instance)
-        unless parse_result.failure?
-          return construct_and_memoize_result(parse_result, alt, parser_instance)
+      def terminal?
+        false
+      end
+    
+      def add_alternative(alt)
+        self.alternatives ||= Array.new
+        alt.containing_nonterminal = self
+        alternatives << alt
+      end
+
+      def parse_at(start, buffer, parser_instance)
+        if cached_result = parser_instance.node_cache.node_starting_at(self.name, start)
+          return cached_result
         end
+      
+        for alt in alternatives do
+          parse_result = alt.parse_at(start, buffer, parser_instance)
+          unless parse_result.failure?
+            return construct_and_memoize_result(parse_result, alt, parser_instance)
+          end
+        end
+        return ParseResult.new_failure(start)
       end
-      return ParseResult.new_failure(start)
-    end
     
-    def get_sibling(name)
-      containing_parser.get_nonterminal(name)
-    end
+      def get_sibling(name)
+        containing_parser.get_nonterminal(name)
+      end
     
-    def def_shared_methods(&methods_block)
-      @has_shared_methods = true
-      class_eval &methods_block
-    end
+      def def_shared_methods(&methods_block)
+        @has_shared_methods = true
+        class_eval &methods_block
+      end
         
-    private
-    def construct_and_memoize_result(result, alternative, parser_instance)
-      value = self.new(result.value)
-      value.extend(alternative.exclusive_methods_module)
-      new_parse_result = ParseResult.new(value, result.start_index, result.end_index)
-      parser_instance.node_cache.store_node(self.name, new_parse_result)
-      return new_parse_result
+      private
+      def construct_and_memoize_result(result, alternative, parser_instance)
+        value = self.new(result.value)
+        value.extend(alternative.exclusive_methods_module)
+        new_parse_result = ParseResult.new(value, result.start_index, result.end_index)
+        parser_instance.node_cache.store_node(self.name, new_parse_result)
+        return new_parse_result
+      end
     end
+
+    module NonterminalInstanceMethods
+      attr_accessor :children
+
+      def initialize(children=[])
+        self.children = children
+      end
+
+      def [](index)
+        children[index]
+      end
+    end  
   end
-
-  module NonterminalInstanceMethods
-    attr_accessor :children
-
-    def initialize(children=[])
-      self.children = children
-    end
-
-    def [](index)
-      children[index]
-    end
-  end  
 end
-
