41252b2a1afadef533c620e08b132ac8e3fc97e5
nathansobo <nathansobo@0990e5dc-4a6a-4c17-b0d6-4743f9744c17>
2007-01-04 04:40:06 +0000

    More of previous refactoring
    git-svn-id: svn+ssh://rubyforge.org/var/svn/treetop@23 0990e5dc-4a6a-4c17-b0d6-4743f9744c17

--- trunk/test/and_predicate_spec.rb
+++ trunk/test/and_predicate_spec.rb
@@ -4,17 +4,17 @@ require 'spec/runner'
 dir = File.dirname(__FILE__)
 require "#{dir}/spec_helper"
 
-context "An and predicate for a TerminalSymbol" do
+context "An &-predicate for a TerminalSymbol" do
   setup do
     @terminal = TerminalSymbol.new("foo")
     @and_predicate = AndPredicate.new(@terminal)
   end
   
-  specify "returns a SyntaxNode without updating the index on a parse of matching input" do
+  specify "returns success without updating the index on a parse of matching input" do
     input = @terminal.prefix
     index = 0
     result = @and_predicate.parse_at(input, index, mock("Parser"))
-    result.should_be_an_instance_of SyntaxNode
+    result.should_be_success
     result.interval.end.should_equal index
   end
   
