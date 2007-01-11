532d9bb5392d43175746c3c20922e9237d9592c4
nathansobo <nathansobo@0990e5dc-4a6a-4c17-b0d6-4743f9744c17>
2007-01-11 05:57:11 +0000

    Method renamed for clarity in Grammar.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/treetop@32 0990e5dc-4a6a-4c17-b0d6-4743f9744c17

--- trunk/lib/treetop/grammar.rb
+++ trunk/lib/treetop/grammar.rb
@@ -16,7 +16,7 @@ module Treetop
     end
     
     def add_parsing_rule(parsing_rule_or_nonterminal, expression = nil)
-      rule = rule_for_args(parsing_rule_or_nonterminal, expression)
+      rule = make_parsing_rule(parsing_rule_or_nonterminal, expression)
       @parsing_rules[rule.nonterminal_symbol.name] = rule
       self.root ||= rule.nonterminal_symbol
     end
@@ -26,7 +26,7 @@ module Treetop
     end
     
     private
-    def rule_for_args(rule_or_nonterminal, expression)
+    def make_parsing_rule(rule_or_nonterminal, expression)
       if rule_or_nonterminal.is_a? NonterminalSymbol
         return ParsingRule.new(rule_or_nonterminal, expression)
       else
