648a7275fa0e19a582dfb34c6dc63ef8d50ea7e8
nathansobo <nathansobo@0990e5dc-4a6a-4c17-b0d6-4743f9744c17>
2007-01-17 07:04:24 +0000

    Added functional spec of meta grammar with a test of terminal symbol parsing.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/treetop@39 0990e5dc-4a6a-4c17-b0d6-4743f9744c17

---
+++ trunk/test/metagrammar_functional_spec.rb
@@ -0,0 +1,37 @@
+require 'rubygems'
+require 'spec/runner'
+
+dir = File.dirname(__FILE__)
+require "#{dir}/spec_helper"
+
+context "A parsing rule for terminal symbols" do
+  setup do
+    metagrammar = Grammar.new do
+      root :terminal_symbol
+      
+      parsing_rule :terminal_symbol do
+        double_quoted_string_char =
+          seq(not('"'),
+              choice(seq('\\', '"'), anything))
+        double_quoted_string =
+          seq('"', zero_or_more(double_quoted_string_char), '"')
+          
+        single_quoted_string_char =
+            seq(not("'"),
+                choice(seq("\\", "'"), anything))
+        single_quoted_string =
+            seq("'", zero_or_more(single_quoted_string_char), "'")
+        
+        choice(single_quoted_string, double_quoted_string)
+      end
+    end
+    
+    @parser = metagrammar.new_parser
+  end
+  
+  specify "parses a single quoted string as a TerminalSymbol with the correct prefix value" do
+    terminal = parser.parse("'foo'").value
+    terminal.should_be_an_instance_of TerminalSymbol
+    terminal.prefix.should_eql 'foo'
+  end
+end
