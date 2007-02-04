ae7c2df3afd980174e1f0e5b32454ba27bc3f9d3
nathansobo <nathansobo@0990e5dc-4a6a-4c17-b0d6-4743f9744c17>
2007-02-04 03:18:33 +0000

    Moved grammar related source files into their own directory.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/treetop@54 0990e5dc-4a6a-4c17-b0d6-4743f9744c17

--- trunk/lib/treetop.rb
+++ trunk/lib/treetop.rb
@@ -1,11 +1,7 @@
 dir = File.dirname(__FILE__)
 require "#{dir}/treetop/grammar"
 require "#{dir}/treetop/metagrammar"
-require "#{dir}/treetop/grammar_builder"
-require "#{dir}/treetop/parsing_expression_builder_helper"
 require "#{dir}/treetop/parser"
-require "#{dir}/treetop/parsing_rule"
-require "#{dir}/treetop/parsing_expressions"
 require "#{dir}/treetop/syntax_node"
 require "#{dir}/treetop/terminal_syntax_node"
 require "#{dir}/treetop/sequence_syntax_node"
---
+++ trunk/lib/treetop/grammar/grammar.rb
@@ -0,0 +1,43 @@
+module Treetop
+  class Grammar
+    attr_accessor :root, :builder
+    
+    def initialize(&block)
+      @parsing_rules = Hash.new
+      @nonterminal_symbols = Hash.new
+      self.builder = GrammarBuilder.new(self)
+      build &block if block
+    end
+
+    def new_parser
+      Parser.new(self)
+    end
+    
+    def nonterminal_symbol(ruby_sym)
+      @nonterminal_symbols[ruby_sym] ||= NonterminalSymbol.new(ruby_sym, self)
+    end
+    
+    def add_parsing_rule(parsing_rule_or_nonterminal, expression = nil)
+      rule = make_parsing_rule(parsing_rule_or_nonterminal, expression)
+      @parsing_rules[rule.nonterminal_symbol.name] = rule
+      self.root ||= rule.nonterminal_symbol
+    end
+    
+    def get_parsing_expression(nonterminal_symbol)
+      @parsing_rules[nonterminal_symbol.name].parsing_expression if @parsing_rules[nonterminal_symbol.name]
+    end
+    
+    def build(&block)
+      builder.build &block
+    end
+    
+    private
+    def make_parsing_rule(rule_or_nonterminal, expression)
+      if rule_or_nonterminal.is_a? NonterminalSymbol
+        return ParsingRule.new(rule_or_nonterminal, expression)
+      else
+        return rule_or_nonterminal
+      end
+    end
+  end
+end
