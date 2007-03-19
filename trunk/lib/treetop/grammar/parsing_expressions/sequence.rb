ec9576c54e0be58e18a4d38eb89f70998cf39d61
da3mon <da3mon@0990e5dc-4a6a-4c17-b0d6-4743f9744c17>
2007-03-19 05:57:43 +0000

    dmc/ns - refactored sequence parse_at and parse_at_without_caching.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/treetop@85 0990e5dc-4a6a-4c17-b0d6-4743f9744c17

--- trunk/lib/treetop/grammar/parsing_expressions/sequence.rb
+++ trunk/lib/treetop/grammar/parsing_expressions/sequence.rb
@@ -13,10 +13,7 @@ module Treetop
     
     def parse_at(input, start_index, parser)
       node_cache = node_cache(parser)
-      if stored_node = node_cache[start_index]
-        return stored_node
-      end
-      node_cache.store(parse_at_without_caching(input, start_index, parser))
+      node_cache[start_index] || node_cache.store(parse_at_without_caching(input, start_index, parser))
     end
     
     def parse_at_without_caching(input, start_index, parser)
@@ -26,11 +23,8 @@ module Treetop
       for elt in elements
         result = elt.parse_at(input, next_index, parser)
         results << result
-        if result.failure?
-          return failure_at(start_index, collect_nested_failures(results))
-        else
-          next_index = result.interval.end
-        end
+        return failure_at(start_index, collect_nested_failures(results)) if result.failure?
+        next_index = result.interval.end
       end
     
       interval = start_index...next_index      
