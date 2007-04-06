36277f59837729cac00b54ec49935304ae6aa17e
nathansobo <nathansobo@0990e5dc-4a6a-4c17-b0d6-4743f9744c17>
2007-04-06 16:32:04 +0000

    ns - finished delimited sequence refactoring
    git-svn-id: svn+ssh://rubyforge.org/var/svn/treetop@95 0990e5dc-4a6a-4c17-b0d6-4743f9744c17

--- trunk/lib/treetop/grammar/parsing_expression_builder_helper.rb
+++ trunk/lib/treetop/grammar/parsing_expression_builder_helper.rb
@@ -62,51 +62,9 @@ module ParsingExpressionBuilderHelper
   def one_or_more(expression)
     exp(expression).one_or_more
   end
-
+  
   def escaped(character)
     seq('\\', character)
-  end
-
-  def n_or_more_delimited(n, expression, delimiter, &block)
-    expression = exp(expression)
-    delimiter = exp(delimiter)
-    
-    head_element = delimited_sequence_head_element(n, expression)
-    tail_element = delimited_sequence_tail_element(n, expression, delimiter)
-    
-    if n > 1
-      tail_elements = one_or_more(tail_element)
-    else
-      tail_elements = zero_or_more(tail_element)
-    end
-    
-    tail_elements.node_class_eval do
-      def elements
-        super.map(&:element)
-      end
-    end
-    
-    delimited_sequence = seq(head_element, tail_elements) do
-      def elements
-        return [] if super[0].empty?
-        [super[0]] + super[1].elements
-      end
-    end
-    
-    delimited_sequence.node_class_eval(&block) if block
-    return delimited_sequence
-  end
-  
-  def delimited_sequence_head_element(n, expression)
-    n == 0 ? optional(expression) : expression
-  end
-  
-  def delimited_sequence_tail_element(n, expression, delimiter)
-    seq(delimiter, expression) do
-      def element
-        elements[1]
-      end
-    end
   end
   
   def zero_or_more_delimited(expression, delimiter, &block)
@@ -115,5 +73,47 @@ module ParsingExpressionBuilderHelper
   
   def two_or_more_delimited(expression, delimiter, &block)
     n_or_more_delimited(2, expression, delimiter, &block)    
+  end
+
+  def n_or_more_delimited(n, expression, delimiter, &block)
+    expression = exp(expression)
+    delimiter = exp(delimiter)
+        
+    delimited_sequence = seq(delimited_sequence_head(n, expression), 
+                             delimited_sequence_tail(n, expression, delimiter)) do
+      def elements
+        return [] if super[0].empty?
+        [super[0]] + super[1].elements
+      end
+    end  
+    delimited_sequence.node_class_eval(&block) if block
+
+    return delimited_sequence
+  end
+  
+  def delimited_sequence_head(n, expression)
+    n == 0 ? optional(expression) : expression
+  end
+    
+  def delimited_sequence_tail(n, expression, delimiter)
+    if n > 1
+      tail_elements = one_or_more(delimited_sequence_tail_element(n, expression, delimiter))
+    else
+      tail_elements = zero_or_more(delimited_sequence_tail_element(n, expression, delimiter))
+    end
+    tail_elements.node_class_eval do
+      def elements
+        super.map(&:element)
+      end
+    end
+    return tail_elements
+  end
+
+  def delimited_sequence_tail_element(n, expression, delimiter)
+    seq(delimiter, expression) do
+      def element
+        elements[1]
+      end
+    end
   end
 end
