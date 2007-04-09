3fb78ed4e6009562d2696592d4f41cd12b61f036
nathansobo <nathansobo@0990e5dc-4a6a-4c17-b0d6-4743f9744c17>
2007-04-09 03:33:45 +0000

    ns - Trailing blocks parse in metagrammar
    git-svn-id: svn+ssh://rubyforge.org/var/svn/treetop@117 0990e5dc-4a6a-4c17-b0d6-4743f9744c17

--- trunk/spec/metagrammar/trailing_block_rule_functional_spec.rb
+++ trunk/spec/metagrammar/trailing_block_rule_functional_spec.rb
@@ -10,30 +10,31 @@ context "The subset of the metagrammar rooted at the trailing_block rule" do
   include MetagrammarSpecContextHelper
   
   setup do
-    @metagrammar = Protometagrammar.new
-    @parser = @metagrammar.new_parser
-    @metagrammar.root = @metagrammar.nonterminal_symbol(:trailing_block)
+    @root = :trailing_block
   end
   
   specify "parses space followed by a node_class_eval block as a node that can node class eval the block's contents on a preceding expression" do
-    block_contents = "\ndef a_method\n\nend\n"
-    result = @parser.parse("   {#{block_contents}}")
-    result.should be_success
+    with_both_protometagrammar_and_metagrammar do
+      block_contents = "\ndef a_method\n\nend\n"
+      result = @parser.parse("   {#{block_contents}}")
+      result.should be_success
         
-    parsing_expression = mock('parsing expression preceding the block')
-    parsing_expression.should_receive(:node_class_eval).with(block_contents)
+      parsing_expression = mock('parsing expression preceding the block')
+      parsing_expression.should_receive(:node_class_eval).with(block_contents)
     
-    result.value(parsing_expression).should == parsing_expression
+      result.value(parsing_expression).should == parsing_expression
+    end
   end
   
   specify "parses nothing as a node that passes the value of the preceding expression through unchanged" do
-    result = @parser.parse("")
-    result.should be_success
+    with_both_protometagrammar_and_metagrammar do
+      result = @parser.parse("")
+      result.should be_success
 
-    parsing_expression = mock('parsing expression preceding the block')
-    parsing_expression.should_not_receive(:node_class_eval)
-    
-    result.value(parsing_expression).should == parsing_expression
+      parsing_expression = mock('parsing expression preceding the block')
+      parsing_expression.should_not_receive(:node_class_eval)
+
+      result.value(parsing_expression).should == parsing_expression
+    end
   end
-  
 end
