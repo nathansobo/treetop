e09fcca26a1026d24b23d552bac438598a5f14b2
nathansobo <nathansobo@0990e5dc-4a6a-4c17-b0d6-4743f9744c17>
2007-04-09 01:39:32 +0000

    NS - Started the real metagrammar's bootstrapping
    git-svn-id: svn+ssh://rubyforge.org/var/svn/treetop@110 0990e5dc-4a6a-4c17-b0d6-4743f9744c17

--- trunk/lib/treetop/grammar/grammar_builder.rb
+++ trunk/lib/treetop/grammar/grammar_builder.rb
@@ -1,33 +1,35 @@
-class GrammarBuilder
-  attr_accessor :grammar
-  include ParsingExpressionBuilderHelper
+module Treetop
+  class GrammarBuilder
+    attr_accessor :grammar
+    include ParsingExpressionBuilderHelper
   
-  def initialize(grammar)
-    self.grammar = grammar
-  end
+    def initialize(grammar)
+      self.grammar = grammar
+    end
   
-  def build(&block)
-    instance_eval(&block)
-  end
+    def build(&block)
+      instance_eval(&block)
+    end
       
-  def root(sym)
-    grammar.root = grammar.nonterminal_symbol(sym)
-  end
+    def root(sym)
+      grammar.root = grammar.nonterminal_symbol(sym)
+    end
   
-  def rule(nonterminal_name, expression_or_expression_builder)
-    nonterminal_symbol = grammar.nonterminal_symbol(nonterminal_name)
-    parsing_expression = parsing_expression_for(expression_or_expression_builder)
+    def rule(nonterminal_name, expression_or_expression_builder)
+      nonterminal_symbol = grammar.nonterminal_symbol(nonterminal_name)
+      parsing_expression = parsing_expression_for(expression_or_expression_builder)
 
-    grammar.add_parsing_rule(nonterminal_symbol, parsing_expression)
-  end
+      grammar.add_parsing_rule(nonterminal_symbol, parsing_expression)
+    end
   
-  def parsing_expression_for(expression_or_expression_builder)
-    case expression_or_expression_builder
-    when ParsingExpression
-      return expression_or_expression_builder
-    when ParsingExpressionBuilder
-      expression_or_expression_builder.grammar = grammar
-      return expression_or_expression_builder.build
+    def parsing_expression_for(expression_or_expression_builder)
+      case expression_or_expression_builder
+      when ParsingExpression
+        return expression_or_expression_builder
+      when ParsingExpressionBuilder
+        expression_or_expression_builder.grammar = grammar
+        return expression_or_expression_builder.build
+      end
     end
   end
 end
--- trunk/spec/metagrammar/metagrammar_spec_context_helper.rb
+++ trunk/spec/metagrammar/metagrammar_spec_context_helper.rb
@@ -8,5 +8,17 @@ module MetagrammarSpecContextHelper
     else
       result.value(grammar)
     end
-  end  
+  end
+  
+  def with_both_protometagrammar_and_metagrammar_rooted_at(nonterminal_symbol)
+    @metagrammar = Protometagrammar.new
+    @parser = @metagrammar.new_parser
+    @metagrammar.root = @metagrammar.nonterminal_symbol(nonterminal_symbol)
+    yield
+    
+    @metagrammar = Metagrammar
+    @parser = @metagrammar.new_parser
+    @metagrammar.root = @metagrammar.nonterminal_symbol(nonterminal_symbol)
+    yield
+  end
 end
