6a7a9ff3d196806f9e54dbb8506a2a266d6b4b59
nathansobo <nathansobo@0990e5dc-4a6a-4c17-b0d6-4743f9744c17>
2007-04-09 00:58:39 +0000

    ns - Begin renaming of current metagrammar to protometagrammar
    git-svn-id: svn+ssh://rubyforge.org/var/svn/treetop@105 0990e5dc-4a6a-4c17-b0d6-4743f9744c17

--- trunk/lib/treetop.rb
+++ trunk/lib/treetop.rb
@@ -1,5 +1,5 @@
 dir = File.dirname(__FILE__)
 require "#{dir}/treetop/grammar"
-require "#{dir}/treetop/metagrammar"
+require "#{dir}/treetop/protometagrammar"
 require "#{dir}/treetop/parser"
 require "#{dir}/treetop/ruby_extension"
