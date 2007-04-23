6f60a0c476ea3c914524819c5e4838b2604aaef7
nathansobo <nathansobo@0990e5dc-4a6a-4c17-b0d6-4743f9744c17>
2007-04-23 05:12:29 +0000

    ns - Nonterminal symbols integrated with new parse result scheme
    git-svn-id: svn+ssh://rubyforge.org/var/svn/treetop@150 0990e5dc-4a6a-4c17-b0d6-4743f9744c17

--- trunk/lib/treetop/grammar/parsing_expressions/nonterminal_symbol.rb
+++ trunk/lib/treetop/grammar/parsing_expressions/nonterminal_symbol.rb
@@ -17,7 +17,13 @@ module Treetop
 
     protected
     def parse_at_without_caching(input, start_index, parser)
-      parsing_expression.parse_at(input, start_index, parser)
+      result = parsing_expression.parse_at(input, start_index, parser)
+      
+      if result.success?
+        SuccessfulParseResult.new(self, result.value, collect_failure_subtrees([result]))
+      else
+        failure_at(start_index, [result])
+      end
     end    
   end
 end
