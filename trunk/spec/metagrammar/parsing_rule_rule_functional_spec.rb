78c5de2679586909a88f5bccc984eff2ab6633ae
nathansobo <nathansobo@0990e5dc-4a6a-4c17-b0d6-4743f9744c17>
2007-06-11 17:09:06 +0000

    ns - Parsing rule nodes in the Metagrammar can generate the Ruby to add themselves to a grammar
    git-svn-id: svn+ssh://rubyforge.org/var/svn/treetop@212 0990e5dc-4a6a-4c17-b0d6-4743f9744c17

--- trunk/spec/metagrammar/parsing_rule_rule_functional_spec.rb
+++ trunk/spec/metagrammar/parsing_rule_rule_functional_spec.rb
@@ -15,7 +15,7 @@ describe "The subset of the metagrammar rooted at the parsing_rule rule" do
    
       result.should be_an_instance_of(ParsingRule)
       result.nonterminal_symbol.name.should == :foo
-      result.parsing_expression.should be_an_instance_of(TerminalSymbol)      
+      result.parsing_expression.should be_an_instance_of(TerminalSymbol)
     end
   end
   
@@ -23,7 +23,7 @@ describe "The subset of the metagrammar rooted at the parsing_rule rule" do
     with_both_protometagrammar_and_metagrammar(@root) do |parser|
       result = parse_result_for(parser, "rule foo 'bar' baz+ (xyzzy / plugh*) !bar end")
    
-      result.should be_an_instance_of(ParsingRule)      
+      result.should be_an_instance_of(ParsingRule)
     end
   end
 end
