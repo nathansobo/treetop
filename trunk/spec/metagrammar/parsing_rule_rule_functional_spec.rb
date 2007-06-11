7bc2c5aef0c6ef6dc2042664b40c2d340a96bd0f
nathansobo <nathansobo@0990e5dc-4a6a-4c17-b0d6-4743f9744c17>
2007-06-11 16:50:31 +0000

    ns - Removed meaningless spec
    git-svn-id: svn+ssh://rubyforge.org/var/svn/treetop@209 0990e5dc-4a6a-4c17-b0d6-4743f9744c17

--- trunk/spec/metagrammar/parsing_rule_rule_functional_spec.rb
+++ trunk/spec/metagrammar/parsing_rule_rule_functional_spec.rb
@@ -9,14 +9,6 @@ describe "The subset of the metagrammar rooted at the parsing_rule rule" do
     @root = :parsing_rule
   end
 
-  it "parses an empty string" do
-    with_both_protometagrammar_and_metagrammar(@root) do |parser|
-      result = parse_result_for(parser, "rule foo 'bar' end")
-   
-      result.should be_an_instance_of(ParsingRule)
-    end
-  end
-  
   it "parses a parse rule with a terminal symbol as its expression" do
     with_both_protometagrammar_and_metagrammar(@root) do |parser|
       result = parse_result_for(parser, "rule foo 'bar' end")
