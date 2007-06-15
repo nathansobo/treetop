4e1960f8de0cd2096292cdb9facf77540f62f730
nathansobo <nathansobo@0990e5dc-4a6a-4c17-b0d6-4743f9744c17>
2007-06-15 17:10:00 +0000

    ns - to_ruby added to all nodes of the Metagrammar
    git-svn-id: svn+ssh://rubyforge.org/var/svn/treetop@215 0990e5dc-4a6a-4c17-b0d6-4743f9744c17

--- trunk/spec/metagrammar/parsing_rule_sequence_rule_functional_spec.rb
+++ trunk/spec/metagrammar/parsing_rule_sequence_rule_functional_spec.rb
@@ -56,6 +56,8 @@ describe "The subset of the metagrammar rooted at the parsing_rule_sequence rule
 end
 
 describe "In the Metagrammar only, the node returned by the parsing_rule_sequence rule's successful parsing of two parsing rules" do
+  include MetagrammarSpecContextHelper
+  
   before do
     with_metagrammar(:parsing_rule_sequence) do |parser|
       input =
@@ -72,5 +74,21 @@ describe "In the Metagrammar only, the node returned by the parsing_rule_sequenc
     
   end
   
-  it "has a Ruby source representation"
+  it "has a Ruby source representation" do
+    @node.to_ruby(grammar_node_mock('Foo')).should == "Foo.add_parsing_rule(ParsingRule.new(Foo.nonterminal_symbol(:foo), TerminalSymbol.new('bar')))\nFoo.add_parsing_rule(ParsingRule.new(Foo.nonterminal_symbol(:baz), TerminalSymbol.new('bot')))\n"
+  end
+end
+
+describe "In the Metagrammar only, the node returned by the parsing_rule_sequence rule's successful parsing of epsilon" do
+  include MetagrammarSpecContextHelper
+  
+  before do
+    with_metagrammar(:parsing_rule_sequence) do |parser|
+      @node = parser.parse('')
+    end
+  end
+  
+  it "has a Ruby source representation" do
+    @node.to_ruby(grammar_node_mock).should == ''
+  end
 end
