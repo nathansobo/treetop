57486855d06d4463ec66d4e757373dd53bc609f3
nathansobo <nathansobo@0990e5dc-4a6a-4c17-b0d6-4743f9744c17>
2007-06-15 23:15:43 +0000

    ns - Separated proto/meta parse rule rule specs.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/treetop@233 0990e5dc-4a6a-4c17-b0d6-4743f9744c17

---
+++ trunk/spec/protometagrammar/parsing_rule_rule_spec.rb
@@ -0,0 +1,29 @@
+dir = File.dirname(__FILE__)
+require "#{dir}/../spec_helper"
+require "#{dir}/protometagrammar_spec_context_helper"
+
+describe "The subset of the Protometagrammar rooted at the parsing_rule rule" do
+  include ProtometagrammarSpecContextHelper
+  
+  before do
+    @root = :parsing_rule
+  end
+
+  it "parses a parse rule with a terminal symbol as its expression" do
+    with_protometagrammar(@root) do |parser|
+      result = parse_result_for(parser, "rule foo 'bar' end")
+   
+      result.should be_an_instance_of(ParsingRule)
+      result.nonterminal_symbol.name.should == :foo
+      result.parsing_expression.should be_an_instance_of(TerminalSymbol)
+    end
+  end
+  
+  it "parses a parse rule with a complex expression" do
+    with_protometagrammar(@root) do |parser|
+      result = parse_result_for(parser, "rule foo 'bar' baz+ (xyzzy / plugh*) !bar end")
+   
+      result.should be_an_instance_of(ParsingRule)
+    end
+  end
+end
