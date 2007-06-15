0c677974f26dda8b187073c38f426c4372286940
nathansobo <nathansobo@0990e5dc-4a6a-4c17-b0d6-4743f9744c17>
2007-06-15 22:39:06 +0000

    ns Split proto/meta sequence rule spec.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/treetop@230 0990e5dc-4a6a-4c17-b0d6-4743f9744c17

--- trunk/spec/protometagrammar/sequence_rule_spec.rb
+++ trunk/spec/protometagrammar/sequence_rule_spec.rb
@@ -1,16 +1,16 @@
 dir = File.dirname(__FILE__)
 require "#{dir}/../spec_helper"
-require "#{dir}/metagrammar_spec_context_helper"
+require "#{dir}/protometagrammar_spec_context_helper"
 
 describe "The subset of the metagrammar rooted at the sequence rule" do
-  include MetagrammarSpecContextHelper
+  include ProtometagrammarSpecContextHelper
   
   before do
     @root = :sequence
   end
 
   it "parses a series of space-separated terminals and nonterminals as a sequence" do
-    with_both_protometagrammar_and_metagrammar(@root) do |parser|
+    with_protometagrammar(@root) do |parser|
       syntax_node = parser.parse('"terminal" nonterminal1 nonterminal2')
       syntax_node.should be_success  
 
@@ -27,7 +27,7 @@ describe "The subset of the metagrammar rooted at the sequence rule" do
   end
   
   it "parses a series of space-separated non-terminals as a sequence" do
-    with_both_protometagrammar_and_metagrammar(@root) do |parser|
+    with_protometagrammar(@root) do |parser|
       syntax_node = parser.parse('a b c')
 
       grammar = Grammar.new
@@ -37,7 +37,7 @@ describe "The subset of the metagrammar rooted at the sequence rule" do
   end
   
   it "node class evaluates a block following a sequence in the parsing expression for that sequence" do
-    with_both_protometagrammar_and_metagrammar(@root) do |parser|
+    with_protometagrammar(@root) do |parser|
       result = parser.parse("a b c {\n  def a_method\n  end\n}")
       result.should be_success
     
@@ -49,7 +49,7 @@ describe "The subset of the metagrammar rooted at the sequence rule" do
   end
   
   it "binds trailing blocks more tightly to terminal symbols than sequences" do
-    with_both_protometagrammar_and_metagrammar(@root) do |parser|
+    with_protometagrammar(@root) do |parser|
       result = parser.parse("a b 'c' {\n  def a_method\n  end\n}")
       result.should be_success
     
@@ -60,47 +60,4 @@ describe "The subset of the metagrammar rooted at the sequence rule" do
       sequence.elements[2].node_class.instance_methods.should include('a_method')      
     end
   end
-end
-
-describe "In the Metagrammar only, the node returned by the sequence rule's successful parsing of space-separated terminals" do
-  include MetagrammarSpecContextHelper
-
-  before do
-    with_metagrammar(:sequence) do |parser|
-      @node = parser.parse("'foo' 'bar' 'baz'")
-    end
-  end
-  
-  it "has a Ruby source representation" do
-    @node.to_ruby(grammar_node_mock).should == "Sequence.new([TerminalSymbol.new('foo'), TerminalSymbol.new('bar'), TerminalSymbol.new('baz')])"
-  end
-end
-
-describe "In the Metagrammar only, the node returned by the sequence rule's successful parsing of space-separated terminals and nonterminals" do
-  include MetagrammarSpecContextHelper
-
-  before do
-    with_metagrammar(:sequence) do |parser|
-      @node = parser.parse("'foo' bar baz")
-    end
-  end
-  
-  it "has a Ruby source representation" do
-    @node.to_ruby(grammar_node_mock('Foo')).should == "Sequence.new([TerminalSymbol.new('foo'), Foo.nonterminal_symbol(:bar), Foo.nonterminal_symbol(:baz)])"
-  end
-end
-
-describe "In the Metagrammar only, a sequence followed by a block" do
-  include MetagrammarSpecContextHelper
-
-  before do
-    with_metagrammar(:sequence) do |parser|
-      @node = parser.parse("foo bar {\ndef a_method\n\nend\n}")
-    end
-  end
-  
-  it "has a Ruby source representation" do
-    @node.to_ruby(grammar_node_mock('Foo')).should == "Sequence.new([Foo.nonterminal_symbol(:foo), Foo.nonterminal_symbol(:bar)]) do\ndef a_method\n\nend\nend"
-  end
-
 end
