592e0014140670ac4ea5188d9da49209f9b171b7
nathansobo <nathansobo@0990e5dc-4a6a-4c17-b0d6-4743f9744c17>
2007-06-15 19:19:13 +0000

    ns - Refactored keyword rule to only match keywords that aren't followed by non-space characters
    git-svn-id: svn+ssh://rubyforge.org/var/svn/treetop@223 0990e5dc-4a6a-4c17-b0d6-4743f9744c17

--- trunk/spec/metagrammar/keyword_rule_functional_spec.rb
+++ trunk/spec/metagrammar/keyword_rule_functional_spec.rb
@@ -2,17 +2,24 @@ dir = File.dirname(__FILE__)
 require "#{dir}/../spec_helper"
 require "#{dir}/metagrammar_spec_context_helper"
 
-describe "The subset of the metagrammar rooted at the keyword rule" do
-  include MetagrammarSpecContextHelper
-  
+describe "The keyword rule's parsing expression" do
   before do
-    @root = :keyword
+    @keyword_expression = Metagrammar.get_parsing_expression(Metagrammar.nonterminal_symbol(:keyword))
   end
   
-  it "parses 'rule' and 'end' successfully" do
-    with_both_protometagrammar_and_metagrammar(@root) do |parser|
-      parser.parse('end').should be_a_success
-      parser.parse('rule').should be_a_success
-    end
+  it "parses 'rule' followed by a space" do
+    @keyword_expression.parse_at('rule ', 0, parser_with_empty_cache_mock).should be_success
+  end
+  
+  it "parses 'end' followed by a space" do
+    @keyword_expression.parse_at('end ', 0, parser_with_empty_cache_mock).should be_success
+  end
+
+  it "parses 'end' followed by a  the end of input" do
+    @keyword_expression.parse_at('end', 0, parser_with_empty_cache_mock).should be_success
+  end
+
+  it "does not parse keywords followed by non-whitespace character" do
+    @keyword_expression.parse_at('rulez', 0, parser_with_empty_cache_mock).should be_failure
   end
 end
