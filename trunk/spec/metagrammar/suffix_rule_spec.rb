d3a95b8df04ecef191787564735aa2fe6c60f62d
nathansobo <nathansobo@0990e5dc-4a6a-4c17-b0d6-4743f9744c17>
2007-06-15 21:06:08 +0000

    ns Split proto/meta suffix spec
    git-svn-id: svn+ssh://rubyforge.org/var/svn/treetop@227 0990e5dc-4a6a-4c17-b0d6-4743f9744c17

--- trunk/spec/metagrammar/suffix_rule_spec.rb
+++ trunk/spec/metagrammar/suffix_rule_spec.rb
@@ -2,50 +2,6 @@ dir = File.dirname(__FILE__)
 require "#{dir}/../spec_helper"
 require "#{dir}/metagrammar_spec_context_helper"
 
-describe "The subset of the metagrammar rooted at the suffix rule" do
-  include MetagrammarSpecContextHelper
-  
-  before do
-    @root = :suffix
-  end
-
-  it "parses a * to a node that can modify the semantics of an expression it follows appropriately" do
-    with_both_protometagrammar_and_metagrammar(@root) do |parser|
-      result = parser.parse('*')
-    
-      parsing_expression = mock('expression preceding the suffix')
-      zero_or_more = mock('zero or more of the parsing expression')
-      parsing_expression.should_receive(:zero_or_more).and_return(zero_or_more)
-    
-      result.value(parsing_expression).should == zero_or_more
-    end
-  end
-
-  it "parses a + node that can modify the semantics of an expression it follows appropriately" do
-    with_both_protometagrammar_and_metagrammar(@root) do |parser|
-      result = parser.parse('+')
-    
-      parsing_expression = mock('expression preceding the suffix')
-      one_or_more = mock('one or more of the parsing expression')
-      parsing_expression.should_receive(:one_or_more).and_return(one_or_more)
-    
-      result.value(parsing_expression).should == one_or_more
-    end
-  end
-
-  it "parses a ? node that can modify the semantics of an expression it follows appropriately" do
-    with_both_protometagrammar_and_metagrammar(@root) do |parser|
-      result = parser.parse('?')
-    
-      parsing_expression = mock('expression preceding the suffix')
-      optional = mock('optional parsing expression')
-      parsing_expression.should_receive(:optional).and_return(optional)
-    
-      result.value(parsing_expression).should == optional
-    end
-  end
-end
-
 describe "In only the Metagrammar, the node returned by the suffix rule's successful parsing of the '+' character" do
   include MetagrammarSpecContextHelper
   
