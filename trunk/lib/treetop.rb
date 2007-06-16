04a6f210319ebaaffea43ecb8295133c961647d2
nathansobo <nathansobo@0990e5dc-4a6a-4c17-b0d6-4743f9744c17>
2007-06-16 01:17:11 +0000

    ns - Added a metagrammar bootstrapping rake task to generate the metagrammar with the protometagrammar, then reparse the metagrammar with that and spit it into a file. Broken tests
    git-svn-id: svn+ssh://rubyforge.org/var/svn/treetop@243 0990e5dc-4a6a-4c17-b0d6-4743f9744c17

--- trunk/lib/treetop.rb
+++ trunk/lib/treetop.rb
@@ -1,9 +1,9 @@
 OPTIMIZE = true
-
 TREETOP_ROOT = File.join(File.dirname(__FILE__), 'treetop')
 
 require "rubygems"
 require "inline"
+
 require "#{TREETOP_ROOT}/parser"
 require "#{TREETOP_ROOT}/grammar"
 require "#{TREETOP_ROOT}/ruby_extension"
