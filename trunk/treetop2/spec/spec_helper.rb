b9038dd5cdc0f9c176ea67818d234003a4cd0ebe
nathansobo <nathansobo@0990e5dc-4a6a-4c17-b0d6-4743f9744c17>
2007-08-04 01:12:29 +0000

    ns - Refactored spec helper out of its indecency
    git-svn-id: svn+ssh://rubyforge.org/var/svn/treetop@337 0990e5dc-4a6a-4c17-b0d6-4743f9744c17

--- trunk/treetop2/spec/spec_helper.rb
+++ trunk/treetop2/spec/spec_helper.rb
@@ -15,18 +15,15 @@ include Treetop2
 class CompilerBehaviour < Spec::DSL::Behaviour
 
   module BehaviorEvalModuleMethods
-    
-    
-    
     def testing_expression(expression_to_test)
       prepend_before(:all) do        
-        setup_test_parser(expression_to_test)
+        setup_test_expression(expression_to_test)
       end
     end
     
     def testing_grammar(grammar_to_test)
       prepend_before(:all) do        
-        setup_test_parser_for_grammar(grammar_to_test)
+        setup_test_grammar(grammar_to_test)
       end
     end
   end
@@ -35,82 +32,73 @@ class CompilerBehaviour < Spec::DSL::Behaviour
     
     attr_reader :parser_class_under_test
     
-    def test_module
-      @test_module ||= Module.new
-    end
-    
     def parse(input, options = {})
-      test_parser = new_test_parser
+      test_parser = parser_class_under_test.new
       test_parser.test_index = options[:at_index] if options[:at_index]
       result = test_parser.parse(input)
       yield result if block_given?
       result
     end
     
-    def new_test_parser
-      parser_class_under_test.new
+    def test_module
+      @test_module ||= Module.new
     end
     
     def teardown_test_module
       @test_module = nil
     end
     
-    def setup_test_parser_for_grammar(grammar_to_test)
-      
-      previous_root = metagrammar.set_root(:grammar)
-      grammar_node = metagrammar_parser.parse(grammar_to_test)
-      metagrammar.set_root(previous_root)
-      
-      if grammar_node.failure?
-        raise "#{grammar_to_test} cannot be parsed by the metagrammar."
-      end      
+    def setup_test_grammar(grammar_to_test)
+      grammar_node = parse_with_metagrammar(grammar_to_test, :grammar)
       test_parser_code = grammar_node.compile
       #puts test_parser_code
       test_module.module_eval(test_parser_code)
       @parser_class_under_test = test_module.const_get(grammar_node.grammar_name.text_value.to_sym)
     end
     
-    def setup_test_parser(expression_to_test)
-      previous_root = metagrammar.set_root(:parsing_expression)
-      expression_node = metagrammar_parser.parse(expression_to_test)
-      metagrammar.set_root(previous_root)
-
-      if expression_node.failure?
-        raise "#{expression_to_test} cannot be parsed by the metagrammar."
-      end
-      
+    def setup_test_expression(expression_to_test)
+      expression_node = parse_with_metagrammar(expression_to_test, :parsing_expression)
+      test_parser_code = generate_test_parser_for_expression(expression_node)
+      #puts test_parser_code
+      test_module.module_eval(test_parser_code)
+      @parser_class_under_test = test_module.const_get(:TestParser)
+    end
+    
+    def generate_test_parser_for_expression(expression_node)
       builder = Compiler::RubyBuilder.new
       lexical_address = builder.next_address
-      
       builder.in(2)
       expression_node.compile(lexical_address, builder)
-      
-      test_parser_code = %{
+      %{
 class TestParser < CompiledParser
   attr_accessor :test_index
-  
+
   def parse(input)
     prepare_to_parse(input)
     return _nt_test_expression
   end
-          
+
   def prepare_to_parse(input)
     self.class.clear_subexpression_procs
     @input = input
     @index = test_index || 0
   end
-          
+
   def _nt_test_expression
 #{builder.ruby}
     return r#{lexical_address}
   end
 end
-}      
-      #puts test_parser_code     
-      test_module.module_eval(test_parser_code)
-      @parser_class_under_test = test_module.const_get(:TestParser)
+}
     end
     
+    def parse_with_metagrammar(input, root = nil)
+      previous_root = metagrammar.set_root(root) if root
+      node = metagrammar_parser.parse(input)
+      metagrammar.set_root(previous_root) if root
+      raise "#{input} cannot be parsed by the metagrammar." if node.failure? 
+      node
+    end
     
     def metagrammar
       Treetop2::Compiler::Metagrammar
