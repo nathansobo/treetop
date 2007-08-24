75eeeaafadb079219dc1fe02ed4e158127551ad0
nathansobo <nathansobo@0990e5dc-4a6a-4c17-b0d6-4743f9744c17>
2007-08-24 21:34:52 +0000

    ns - An epsilon node class expression defaults to SyntaxNode
    git-svn-id: svn+ssh://rubyforge.org/var/svn/treetop@370 0990e5dc-4a6a-4c17-b0d6-4743f9744c17

--- trunk/treetop2/lib/treetop2/compiler/compiler.rb
+++ trunk/treetop2/lib/treetop2/compiler/compiler.rb
@@ -122,7 +122,7 @@ module Treetop2
         use_vars :result, :start_index, :accumulator, :nested_results
         compile_sequence_elements(sequence_elements)
         builder.if__ "#{accumulator_var}.last.success?" do
-          assign_result "(#{node_class_declarations.node_class || 'SyntaxNode'}).new(input, #{start_index_var}...index, #{accumulator_var})"
+          assign_result "(#{node_class_declarations.node_class}).new(input, #{start_index_var}...index, #{accumulator_var})"
         end
         builder.else_ do
           reset_index
