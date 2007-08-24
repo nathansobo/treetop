6626d6e5efd87a8c3943bc3c7ae7df2e3a81fe4f
nathansobo <nathansobo@0990e5dc-4a6a-4c17-b0d6-4743f9744c17>
2007-08-24 22:24:38 +0000

    ns - Zero or more of a terminal tested with inline module
    git-svn-id: svn+ssh://rubyforge.org/var/svn/treetop@375 0990e5dc-4a6a-4c17-b0d6-4743f9744c17

--- trunk/treetop2/test/compiler/zero_or_more_test.rb
+++ trunk/treetop2/test/compiler/zero_or_more_test.rb
@@ -5,12 +5,13 @@ class ZeroOrMoreOfATerminalWithNodeClassDeclarationTest < CompilerTestCase
   class Foo < Treetop2::Parser::SyntaxNode
   end
 
-  testing_expression '"foo"* <Foo>'
+  testing_expression '"foo"* <Foo> { def a_method; end }'
   
   it "successfully parses epsilon, returning an instance declared node class with a nested failure" do
     parse('') do |result|
       result.should be_success
       result.should be_an_instance_of(Foo)
+      result.should respond_to(:a_method)
       result.nested_failures.size.should == 1
       nested_failure = result.nested_failures.first
       nested_failure.index.should == 0
