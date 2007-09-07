5b8bbbc5873fc5a52132cc967ab5ab239b1c186f
nathansobo <nathansobo@0990e5dc-4a6a-4c17-b0d6-4743f9744c17>
2007-09-07 17:43:21 +0000

    ns - Replaced Treetop 1 dependent compiler with self sufficient compiler
    git-svn-id: svn+ssh://rubyforge.org/var/svn/treetop@391 0990e5dc-4a6a-4c17-b0d6-4743f9744c17

--- trunk/treetop2/lib/treetop2/compiler_2.rb
+++
@@ -1,6 +0,0 @@
-dir = File.dirname(__FILE__)
-require File.join(dir, *%w[compiler_2 lexical_address_space])
-require File.join(dir, *%w[compiler_2 ruby_builder])
-require File.join(dir, *%w[compiler_2 parsing_expression])
-require File.join(dir, *%w[compiler_2 compiler])
-require File.join(dir, *%w[compiler_2 metagrammar])
