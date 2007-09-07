379342683cc70756ba5ed5c03486f171f315db86
nathansobo <nathansobo@0990e5dc-4a6a-4c17-b0d6-4743f9744c17>
2007-09-07 18:08:16 +0000

    ns - Forgot to add this
    git-svn-id: svn+ssh://rubyforge.org/var/svn/treetop@393 0990e5dc-4a6a-4c17-b0d6-4743f9744c17

---
+++ trunk/treetop2/lib/treetop2/compiler.rb
@@ -0,0 +1,6 @@
+dir = File.dirname(__FILE__)
+require File.join(dir, *%w[compiler lexical_address_space])
+require File.join(dir, *%w[compiler ruby_builder])
+require File.join(dir, *%w[compiler parsing_expression])
+require File.join(dir, *%w[compiler compiler])
+require File.join(dir, *%w[compiler metagrammar])
