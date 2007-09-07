e10630890d3bfae4833954851a809fad44ab065b
nathansobo <nathansobo@0990e5dc-4a6a-4c17-b0d6-4743f9744c17>
2007-09-07 22:34:13 +0000

    ns - Compiler can choose an output file name explicitly or return string
    git-svn-id: svn+ssh://rubyforge.org/var/svn/treetop@403 0990e5dc-4a6a-4c17-b0d6-4743f9744c17

--- trunk/test/compiler/grammar_compiler_test.rb
+++ trunk/test/compiler/grammar_compiler_test.rb
@@ -4,25 +4,39 @@ class GrammarCompilerTest < Screw::Unit::TestCase
   
   def setup
     @compiler = Compiler::GrammarCompiler.new
-    @source_file_path = File.join(File.dirname(__FILE__), 'test_grammar.treetop')
-    @target_file_path = File.join(File.dirname(__FILE__), 'test_grammar.rb')
-    delete_target_file
+    @source_path = File.join(File.dirname(__FILE__), 'test_grammar.treetop')
+    @target_path = File.join(File.dirname(__FILE__), 'test_grammar.rb')
+    @alternate_target_path = File.join(File.dirname(__FILE__), 'test_grammar_alt.rb')
+    delete_target_files
   end
   
   def teardown
-    delete_target_file
+    delete_target_files
   end
   
-  test "compilation of a single file" do
-    assert !File.exists?(@target_file_path)
-    @compiler.compile(@source_file_path)
-    assert File.exists?(@target_file_path)
-    require @target_file_path
+  test "compilation of a single file to a default file name" do
+    assert !File.exists?(@target_path)
+    @compiler.compile(@source_path)
+    assert File.exists?(@target_path)
+    require @target_path
     Test::Grammar.new.parse('foo').should be_success
   end
   
-  def delete_target_file
-    File.delete(@target_file_path) if File.exists?(@target_file_path)
+  test "compilation of a single file to an explicit file name" do
+    assert !File.exists?(@alternate_target_path)
+    @compiler.compile(@source_path, @alternate_target_path)
+    assert File.exists?(@alternate_target_path)
+    require @alternate_target_path
+    Test::Grammar.new.parse('foo').should be_success
+  end
+  
+  test "compilation of a single file without writing it to an output file" do
+    @compiler.ruby_source(@source_path).should_not be_nil
+  end
+  
+  def delete_target_files
+    File.delete(@target_path) if File.exists?(@target_path)
+    File.delete(@alternate_target_path) if File.exists?(@alternate_target_path)
   end
   
 end
