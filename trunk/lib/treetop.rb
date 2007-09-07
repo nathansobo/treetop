3f17e2d5620d479bf66ad606ee13ddfc28d2b52f
nathansobo <nathansobo@0990e5dc-4a6a-4c17-b0d6-4743f9744c17>
2007-09-07 22:23:34 +0000

    ns - Renamed parser namespace to runtime
    git-svn-id: svn+ssh://rubyforge.org/var/svn/treetop@402 0990e5dc-4a6a-4c17-b0d6-4743f9744c17

--- trunk/lib/treetop.rb
+++ trunk/lib/treetop.rb
@@ -6,5 +6,5 @@ dir = File.dirname(__FILE__)
 
 TREETOP_ROOT = File.join(dir, 'treetop')
 require File.join(TREETOP_ROOT, "ruby_extensions")
-require File.join(TREETOP_ROOT, "parser")
+require File.join(TREETOP_ROOT, "runtime")
 require File.join(TREETOP_ROOT, "compiler")
--- trunk/lib/treetop/parser.rb
+++
@@ -1,9 +0,0 @@
-dir = File.dirname(__FILE__)
-require "#{dir}/parser/compiled_parser"
-require "#{dir}/parser/parse_result"
-require "#{dir}/parser/syntax_node"
-require "#{dir}/parser/parse_failure"
-require "#{dir}/parser/node_cache"
-require "#{dir}/parser/parse_cache"
-require "#{dir}/parser/parse_failure"
-require "#{dir}/parser/terminal_parse_failure"
