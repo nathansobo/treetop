cfdb6421ceb2ebd57d72a6ce8861aba9857d80c7
nathansobo <nathansobo@0990e5dc-4a6a-4c17-b0d6-4743f9744c17>
2007-09-14 17:13:42 +0000

    ns - Fixed composition test failures when run in suite
    git-svn-id: svn+ssh://rubyforge.org/var/svn/treetop@417 0990e5dc-4a6a-4c17-b0d6-4743f9744c17

--- trunk/test/composition/grammar_composition_test.rb
+++ trunk/test/composition/grammar_composition_test.rb
@@ -1,16 +1,16 @@
 dir = File.dirname(__FILE__)
 require File.join(dir, '..', 'test_helper')
 
-load_grammar File.join(dir, 'a')
-load_grammar File.join(dir, 'b')
-load_grammar File.join(dir, 'c')
-load_grammar File.join(dir, 'd')
-
-class GrammarCompositiontest < Screw::Unit::TestCase
-  
+class GrammarCompositionTest < Screw::Unit::TestCase
   def setup
-    @c = Test::CParser.new
-    @d = Test::DParser.new
+    dir = File.dirname(__FILE__)
+    load_grammar File.join(dir, 'a')
+    load_grammar File.join(dir, 'b')
+    load_grammar File.join(dir, 'c')
+    load_grammar File.join(dir, 'd')
+    
+    @c = ::Test::CParser.new
+    @d = ::Test::DParser.new
   end
   
   test "rules in C have access to rules defined in A and B" do
