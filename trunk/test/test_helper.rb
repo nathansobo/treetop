1043700259d8bdf664e01339a93b6784ed3f9cc8
nathansobo <nathansobo@0990e5dc-4a6a-4c17-b0d6-4743f9744c17>
2007-12-02 06:21:09 +0000

    wb/ns - Refactoring on the bootstrapping phase
    git-svn-id: svn+ssh://rubyforge.org/var/svn/treetop@457 0990e5dc-4a6a-4c17-b0d6-4743f9744c17

--- trunk/test/test_helper.rb
+++ trunk/test/test_helper.rb
@@ -2,30 +2,10 @@ dir = File.dirname(__FILE__)
 require 'rubygems'
 require File.expand_path(File.join(dir, 'screw', 'unit'))
 
-unless $bootstrapped_compiler_under_test
-  # Loading trusted version of Treetop to compile the compiler
-  trusted_treetop_path = Gem.source_index.find_name('treetop', ["1.1.2"]).last.full_gem_path
-  require File.join(trusted_treetop_path, 'lib', 'treetop')
-
-  # Relocating trusted version of Treetop to Trusted::Treetop
-  Trusted = Module.new
-  Trusted::Treetop = Treetop
-  Object.send(:remove_const, :Treetop)
-  Object.send(:remove_const, :TREETOP_ROOT)
-
-  # Requiring version of Treetop that is under test
-  require File.expand_path(File.join(dir, '..', 'lib', 'treetop'))
-  # Remove stale Metagrammar defined by the generated metagrammar.rb in system under test
-  Treetop::Compiler.send(:remove_const, :Metagrammar)
-
-  # Compile and evaluate freshly generated metagrammar source
-  METAGRAMMAR_PATH = File.join(TREETOP_ROOT, 'compiler', 'metagrammar.treetop')
-  compiled_metagrammar_source = Trusted::Treetop::Compiler::GrammarCompiler.new.ruby_source(METAGRAMMAR_PATH)
-  Object.class_eval(compiled_metagrammar_source)
-
-  include Treetop
-  $bootstrapped_compiler_under_test = true
+unless $bootstrapped_compiler_being_tested
+  load File.join(dir, 'bootstrap_compiler_being_tested.rb')
 end
+include Treetop
 
 class CompilerTestCase < Screw::Unit::TestCase
   class << self
