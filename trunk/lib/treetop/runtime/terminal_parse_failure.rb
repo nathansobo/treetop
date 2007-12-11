b6a94624a06dfd5a0af8b465cae960c6652cc5e9
Clifford Heath <clifford.heath@gmail.com>
2007-12-12 10:19:28 +1100

    Fixed breakage in TerminalParseFailure#to_s, removed #line and #column * TerminalParseFailure#line and #column were still referencing input * #to_s doesn't report line and column now * New parser failure API will be added in next commit

--- trunk/lib/treetop/runtime/terminal_parse_failure.rb
+++ trunk/lib/treetop/runtime/terminal_parse_failure.rb
@@ -2,23 +2,15 @@ module Treetop
   module Runtime
     class TerminalParseFailure
       attr_reader :index, :expected_string
-      
+
       def initialize(index, expected_string)
         @index = index
         @expected_string = expected_string
       end
 
-      def line
-        input.line_of(index)
-      end
-      
-      def column
-        input.column_of(index)
-      end
-      
       def to_s
-        "String matching #{expected_string} expected at line #{line}, column #{column} (index #{index})."
-      end    
+        "String matching #{expected_string} expected."
+      end
     end
   end
 end
