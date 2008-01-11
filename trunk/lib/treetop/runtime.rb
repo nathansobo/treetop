334a1144dd05407ab7b7d4eadf640622a2eb21c0
Nathan Sobo <nathansobo@gmail.com>
2008-01-11 11:44:27 -0800

    Added Marco Guilmette's TextMate bundle and deleted trunk.

--- trunk/lib/treetop/runtime.rb
+++
@@ -1,6 +0,0 @@
-dir = File.dirname(__FILE__)
-require "#{dir}/runtime/compiled_parser"
-require "#{dir}/runtime/syntax_node"
-require "#{dir}/runtime/node_cache"
-require "#{dir}/runtime/parse_cache"
-require "#{dir}/runtime/terminal_parse_failure"
--- trunk/vendor/rspec/spec/spec/interop/test/unit/testcase_spec_with_test_unit.rb
+++
@@ -1,8 +0,0 @@
-require "test/unit"
-require File.dirname(__FILE__) + '/../../../../spec_helper.rb'
-
-describe "TestCase#method_name" do
-  it "should equal the description of the example" do
-    @method_name.should == "should equal the description of the example"
-  end
-end
