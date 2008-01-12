c294162094cdb098e889dce18c5116140c54181c
Nathan Sobo <nathansobo@gmail.com>
2008-01-11 16:19:07 -0800

    Site is getting closer.

--- doc/site.rb
+++ doc/site.rb
@@ -1,17 +1,21 @@
 require 'rubygems'
 require 'erector'
+require "#{File.dirname(__FILE__)}/sitegen"
 
 class Layout < Erector::Widget
   def render
     html do
       head do
         link :rel => "stylesheet",
-             :type => "text/css",
-             :href => "./screen.css"
+        :type => "text/css",
+        :href => "./screen.css"
       end
-      
+
       body do
         div :id => 'top' do
+          div :id => 'main_navigation' do
+            main_navigation
+          end
         end
         div :id => 'middle' do
           div :id => 'content' do
@@ -19,16 +23,21 @@ class Layout < Erector::Widget
           end
         end
         div :id => 'bottom' do
-          
+
         end
       end
-    end    
-  end
-  
-  def bluecloth(path)
-    File.open(path) do |file|
-      text BlueCloth.new(file.read).to_html
     end
+  end
+
+  def main_navigation
+    ul do
+      li { link_to "Documentation", SyntacticRecognition, Documentation }
+      li { link_to "Contribute", Contribute }
+      li { link_to "Home", Index }
+    end
+  end
+
+  def content
   end
 end
 
@@ -38,4 +47,51 @@ class Index < Layout
   end
 end
 
-puts Index.new.to_s
+class Documentation < Layout
+  abstract
+
+  def content
+    div :id => 'secondary_navigation' do
+      ul do
+        li { link_to 'Syntax', SyntacticRecognition }
+        li { link_to 'Semantics', SemanticInterpretation }
+        li { link_to 'Using In Ruby', UsingInRuby }
+        li { link_to 'Advanced Techniques', PitfallsAndAdvancedTechniques }
+      end
+    end
+
+    documentation_content
+  end
+end
+
+class SyntacticRecognition < Documentation
+  def documentation_content
+    bluecloth "syntactic_recognition.markdown"
+  end
+end
+
+class SemanticInterpretation < Documentation
+  def documentation_content
+    bluecloth "semantic_interpretation.markdown"
+  end
+end
+
+class UsingInRuby < Documentation
+  def documentation_content
+    bluecloth "using_in_ruby.markdown"
+  end
+end
+
+class PitfallsAndAdvancedTechniques < Documentation
+  def documentation_content
+    bluecloth "pitfalls_and_advanced_techniques.markdown"
+  end
+end
+
+
+class Contribute < Layout
+
+end
+
+
+Layout.generate_site
