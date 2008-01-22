b215026464d996ec3710eaec2f4cd105a761c1d9
Nathan Sobo <nathansobo@gmail.com>
2008-01-21 16:26:29 -0800

    Flushed out interval skip list deletion examples

--- spec/runtime/interval_skip_list/delete_spec.rb
+++ spec/runtime/interval_skip_list/delete_spec.rb
@@ -34,6 +34,10 @@ describe IntervalSkipList, " with nodes of height 1, 3, 1, 2, 3" do
       node.key.should == 1
       node.height.should == 1
     end
+
+    it "has :b, :c, :d, :a as its only forward markers at level 0" do
+      node.forward_markers[0].should have_markers(:b, :c, :d, :a)
+    end
   end
 
   describe " nodes[1]" do
@@ -44,6 +48,18 @@ describe IntervalSkipList, " with nodes of height 1, 3, 1, 2, 3" do
     it "has a key of 3 and a height of 3" do
       node.key.should == 3
       node.height.should == 3
+    end
+
+    it "has :a as its only forward marker at level 2" do
+      node.forward_markers[2].should have_marker(:a)
+    end
+
+    it "has :d as its only forward marker at level 1" do
+      node.forward_markers[1].should have_marker(:d)
+    end
+
+    it "has :c as its only forward marker at level 0" do
+      node.forward_markers[0].should have_marker(:c)
     end
   end
 
@@ -56,6 +72,14 @@ describe IntervalSkipList, " with nodes of height 1, 3, 1, 2, 3" do
       node.key.should == 5
       node.height.should == 1
     end
+
+    it "has no forward markers at level 0" do
+      node.forward_markers[0].should be_empty
+    end
+
+    it "has :c as its only marker" do
+      node.markers.should have_marker(:c)
+    end
   end
 
   describe " nodes[3]" do
@@ -67,6 +91,16 @@ describe IntervalSkipList, " with nodes of height 1, 3, 1, 2, 3" do
       node.key.should == 7
       node.height.should == 2
     end
+
+    it "has no forward markers at any level" do
+      node.forward_markers[0].should be_empty
+      node.forward_markers[1].should be_empty
+    end
+
+    it "has :d as its only marker" do
+      node.markers.should have_marker(:d)
+    end
+
   end
 
   describe " nodes[4]" do
@@ -78,6 +112,17 @@ describe IntervalSkipList, " with nodes of height 1, 3, 1, 2, 3" do
       node.key.should == 9
       node.height.should == 3
     end
+
+    it "has no forward markers at any level" do
+      node.forward_markers[0].should be_empty
+      node.forward_markers[1].should be_empty
+      node.forward_markers[2].should be_empty
+    end
+
+    it "has :a as its only marker" do
+      node.markers.should have_marker(:a)
+    end
+
   end
   
 end
