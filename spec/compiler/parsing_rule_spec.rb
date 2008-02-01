00dfebc76f6c628d7893a6e58212ec190769aa7e
Nathan Sobo <nathansobo@gmail.com>
2008-02-01 13:59:18 -0800

    NS/SH - Added some incomplete coverage of the caching of parse failures by parsing rules

--- spec/compiler/parsing_rule_spec.rb
+++ spec/compiler/parsing_rule_spec.rb
@@ -11,12 +11,12 @@ module ParsingRuleSpec
       end
     }
 
-    it "stores and retrieves nodes in its node cache" do
+    it "stores and retrieves successful results for that rule in its node cache, correctly updating the index upon retrieval" do
       parser = self.class.const_get(:FooParser).new
       parser.send(:prepare_to_parse, 'baz')
       node_cache = parser.send(:node_cache)
-    
-      node_cache[:bar][0].should be_nil
+
+      node_cache[:bar].should_not have_key(0)
     
       parser._nt_bar
     
@@ -24,9 +24,26 @@ module ParsingRuleSpec
       cached_node.should be_an_instance_of(Runtime::SyntaxNode)
       cached_node.text_value.should == 'baz'
     
-      parser.instance_eval { @index = 0 }
+      parser.send(:reset_index)
       parser._nt_bar.should equal(cached_node)
       parser.index.should == cached_node.interval.end
+    end
+
+    it "stores and retrieves failed results for that rule in its node cache" do
+      parser = self.class.const_get(:FooParser).new
+      parser.send(:prepare_to_parse, 'bogus')
+      node_cache = parser.send(:node_cache)
+
+      node_cache[:bar].should_not have_key(0)
+
+      parser._nt_bar
+
+      node_cache[:bar].should have_key(0)
+      node_cache[:bar][0].should == nil
+
+      parser.send(:reset_index)
+      parser._nt_bar.should == nil
+      parser.index.should == 0
     end
   end
 end
