4b0b91f4c4866291753cfd7ab16a86f999473f7a
Clifford Heath <clifford.heath@gmail.com>
2009-08-01 19:47:21 +1000

    tt command line revamped, will not overwrite non-tt output file

--- lib/treetop.rb
+++ lib/treetop.rb
@@ -1,5 +1,10 @@
 require 'rubygems'
 
+module Treetop
+  VALID_GRAMMAR_EXT = ['treetop', 'tt']
+  VALID_GRAMMAR_EXT_REGEXP = /\.(#{VALID_GRAMMAR_EXT.join('|')})\Z/o
+end
+
 dir = File.dirname(__FILE__)
 
 TREETOP_ROOT = File.join(dir, 'treetop')
@@ -8,4 +13,4 @@ require File.join(TREETOP_ROOT, "runtime")
 require File.join(TREETOP_ROOT, "compiler")
 
 require 'polyglot'
-Polyglot.register(["treetop", "tt"], Treetop)
+Polyglot.register(Treetop::VALID_GRAMMAR_EXT, Treetop)
