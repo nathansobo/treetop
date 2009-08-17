7329c8026245d5940fecc72eb7472baec718c804
Nicolas Fouché <nicolas@silentale.com>
2009-08-17 15:41:35 +0200

    - Added the support for UTF-8 characters. The patch was originally proposed by hagabaka on github: http://github.com/hagabaka/treetop/commit/aff5a7e08200fbe7d1e047bb101256be377bab92 - All specs still pass. - Oops, checked in a stale (incorrectly merged) file

--- lib/treetop/compiler/node_classes/character_class.rb
+++ lib/treetop/compiler/node_classes/character_class.rb
@@ -5,8 +5,8 @@ module Treetop
         super
         
         builder.if__ "has_terminal?(#{grounded_regexp(text_value)}, true, index)" do
+          builder << 'next_character = index + input[index..-1].match(/\A(.)/um).end(1)'
           if address == 0 || decorated?
-            builder << 'next_character = index + input[index..-1].match(/\A(.)/um).end(1)'
             assign_result "instantiate_node(#{node_class_name}, input, index...next_character)"
             extend_result_with_inline_module
           else
