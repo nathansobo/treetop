2eb685b6ef58ba2254c432889e3dd7bd97505b3d
Jason Garber <jg@jasongarber.com>
2009-11-25 07:46:07 -0500

    Fix parser crash when lazily instantiated node is used from the cache.

--- spec/compiler/parsing_rule_spec.rb
+++ spec/compiler/parsing_rule_spec.rb
@@ -29,4 +29,33 @@ module ParsingRuleSpec
       parser.index.should == cached_node.interval.end
     end
   end
+  
+  
+  describe "a grammar with choice that uses the cache and has a subsequent expression" do    
+    testing_grammar %{
+      grammar Logic
+        rule expression
+          value_plus
+          /
+          value
+        end
+
+        rule value_plus
+          value "something else"
+        end
+
+        rule value
+          [a-z]
+          /
+          "foobar" # the subsequent expression that needs cached.interval.end
+        end
+      end
+    }
+    
+    it "parses a single-character value and generates a node from the cache" do
+      result = parse('a')
+      result.should be_a(Treetop::Runtime::SyntaxNode)
+      result.elements.should be_nil
+    end
+  end
 end
