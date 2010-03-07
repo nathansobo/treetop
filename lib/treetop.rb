ce624b5b5d9786c911916b9bc3527b74572fb92b
Jeremy Kemper <jeremy@bitsweat.net>
2010-03-07 13:22:18 -0800

    Consolidate polyglot support into treetop/polyglot

--- lib/treetop.rb
+++ lib/treetop.rb
@@ -1,10 +1,3 @@
-module Treetop
-  VALID_GRAMMAR_EXT = ['treetop', 'tt']
-  VALID_GRAMMAR_EXT_REGEXP = /\.(#{VALID_GRAMMAR_EXT.join('|')})\Z/o
-end
-
 require 'treetop/runtime'
 require 'treetop/compiler'
-
-require 'polyglot'
-Polyglot.register(Treetop::VALID_GRAMMAR_EXT, Treetop)
+require 'treetop/polyglot'
